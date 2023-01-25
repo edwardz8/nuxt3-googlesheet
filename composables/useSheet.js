f22dfb21cd3f9000e35da592b0bdc59620dc1950
Edwards, Zach <edwardszm@ornl.gov>
2023-01-24 22:57:36 -0500

    init

--- composables/useSheet.js
+++ composables/useSheet.js
@@ -1,15 +1,23 @@
-import { SPREAD_SHEET_ID, GOOGLE_API_KEY } from "@/secrets";
-
 let sheetRange = "Sheet1!A1:D3";
 
+const getVars = () => {
+    const SPREAD_SHEET_ID = useRuntimeConfig().public.SPREAD_SHEET_ID;
+    const GOOGLE_API_KEY = useRuntimeConfig().public.GOOGLE_API_KEY;
+
+    return { SPREAD_SHEET_ID, GOOGLE_API_KEY }
+}
+
 export async function allRows() {
+    const { SPREAD_SHEET_ID, GOOGLE_API_KEY } = getVars();
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${sheetRange}?key=${GOOGLE_API_KEY}`
     return await useFetch(url)
 }
 
 export async function singleRow(row) {
+    const { SPREAD_SHEET_ID, GOOGLE_API_KEY } = getVars();
+
     const rowRange = `Sheet1!A${row}:D${row}`
-    console.log(rowRange)
+   // console.log(rowRange)
 
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREAD_SHEET_ID}/values/${rowRange}?key=${GOOGLE_API_KEY}`
     return await useFetch(url)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     runtimeConfig: {
-        apiKey: process.env.GOOGLE_API_KEY,
-        sheetID: process.env.SPREAD_SHEET_ID
+        public: {
+            GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
+            SPREAD_SHEET_ID: process.env.SPREAD_SHEET_ID
+        }
     }
 })
